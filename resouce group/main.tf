906641e48f33a5adb491bfca5c72b1f28545f92c
Ramakrishnareddy25 <ramakrishnareddytetali@gmail.com>
2026-08-16 17:13:32 +0530

    to add resource group

---
+++ resouce group/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "21dc9d4e-91d5-4da3-b1d1-b08a3b35e94c"
+  features {}
+}
+
+# Variables for Resource Group
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "example-resource-group"
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+  default     = "East US"
+}
+
+# Resource Group definition
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}

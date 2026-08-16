ab10e7fd85a1ac37bdefaacece9ede1d90f5f586
Ramakrishnareddy25 <ramakrishnareddytetali@gmail.com>
2026-08-16 15:38:44 +0530

    deploy

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "aksdevrg"
-    storage_account_name = "tdssto"
+    storage_account_name = "aksdevstg0012504"
     container_name       = "tfstate"
     key                  = "storage.terraform.tfstate"
     # Authenticate using the ARM_* service-principal environment variables.
